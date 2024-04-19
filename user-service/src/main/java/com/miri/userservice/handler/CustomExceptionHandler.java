99053d7486fa672d93524b67406f5b09ace62707
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2024-04-19 14:38:57 +0900

    [user-service] feat: aop 및 예외 핸들러 구현

---
+++ user-service/src/main/java/com/miri/userservice/handler/CustomExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.miri.userservice.handler;
+
+import com.miri.userservice.handler.ex.CustomApiException;
+import com.miri.userservice.handler.ex.EmailAlreadyExistsException;
+import com.miri.userservice.dto.ResponseDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class CustomExceptionHandler {
+
+    @ExceptionHandler(CustomApiException.class)
+    public ResponseEntity<?> apiException(CustomApiException e) {
+
+        log.error(e.getMessage());
+        return new ResponseEntity<>(new ResponseDto<>(-1, e.getMessage(), null), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(EmailAlreadyExistsException.class)
+    public ResponseEntity<?> apiException(EmailAlreadyExistsException e) {
+
+        log.error(e.getMessage());
+        return new ResponseEntity<>(new ResponseDto<>(-1, e.getMessage(), null), HttpStatus.BAD_REQUEST);
+    }
+}
