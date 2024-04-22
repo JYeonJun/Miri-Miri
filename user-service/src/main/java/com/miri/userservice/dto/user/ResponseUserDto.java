82ee8ad268808c73d8d041f8c3c1c7f492665d69
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2024-04-22 14:11:37 +0900

    [user-service] feat: 마이페이지 정보 조회 기능 추가 구현
    - 등록한 상품 목록 조회 기능 추가
    - 위시리스트에 담긴 항품 목록 조회 기능 추가
    - 주문한 상품 목록 조회 기능 추가

--- user-service/src/main/java/com/miri/userservice/dto/user/ResponseUserDto.java
+++ user-service/src/main/java/com/miri/userservice/dto/user/ResponseUserDto.java
@@ -1,6 +1,10 @@
 package com.miri.userservice.dto.user;
 
 import com.miri.userservice.domain.user.User;
+import com.miri.userservice.dto.goods.ResponseGoodsDto.RegisterGoodsListRespDto;
+import com.miri.userservice.dto.order.ResponseOrderDto.OrderGoodsDto;
+import com.miri.userservice.dto.order.ResponseOrderDto.OrderGoodsListRespDto;
+import com.miri.userservice.dto.wishlist.ResponseWishListDto.WishListRespDto;
 import lombok.Data;
 
 public class ResponseUserDto {
@@ -18,16 +22,26 @@ public class ResponseUserDto {
 
     @Data
     public static class GetUserRespDto {
+        private Long userId;
         private String email;
         private String userName;
         private String phoneNumber;
         private String address;
 
-        public GetUserRespDto(User user) {
+        private RegisterGoodsListRespDto registerGoodsList;
+        private WishListRespDto wishList;
+        private OrderGoodsListRespDto orders;
+
+        public GetUserRespDto(User user, RegisterGoodsListRespDto registerGoodsList,
+                              WishListRespDto wishList, OrderGoodsListRespDto orders) {
+            this.userId = user.getId();
             this.email = user.getEmail();
             this.userName = user.getUserName();
             this.phoneNumber = user.getPhoneNumber();
             this.address = user.getAddress();
+            this.registerGoodsList = registerGoodsList;
+            this.wishList = wishList;
+            this.orders = orders;
         }
     }
 }
