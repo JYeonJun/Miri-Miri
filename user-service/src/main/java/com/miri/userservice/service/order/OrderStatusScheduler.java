7a0b96dcbf219d02491b8895c0e31231e7d90928
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2024-04-25 10:54:21 +0900

    docs: OrderStatusScheduler 주석 작성

--- user-service/src/main/java/com/miri/userservice/service/order/OrderStatusScheduler.java
+++ user-service/src/main/java/com/miri/userservice/service/order/OrderStatusScheduler.java
@@ -33,16 +33,20 @@ public class OrderStatusScheduler {
     @Scheduled(cron = "0 0 6 * * *") // 매일 오전 6시에 실행
     @Transactional
     public void updateOrderStatus() {
+        // 터질 수 있으니 페이징 방식처럼 나눠서 처리할 수도 있음(데이터가 많아도 처리 가능)
+        // 스케줄러 3개로 나눠서 처리하는 게 좋다.
         log.info("OrderStatus Update Scheduler Start!!");
-        List<OrderDetail> orderDetails = orderDetailRepository.findByOrderStatusIn(
+        List<OrderDetail> orderDetails = orderDetailRepository.findByOrderStatusIn( // 쿼리 날릴 때 시간도 함께 날려서
                 Arrays.asList(OrderStatus.PENDING, OrderStatus.IN_TRANSIT, OrderStatus.RETURN_IN_PROGRESS));
 
         LocalDate now = LocalDate.now();
 
+        // 차라리 필터를 써서 주문상태별로 나눠서 처리하기
+
         for (OrderDetail orderDetail : orderDetails) {
             LocalDate lastModifiedDate = orderDetail.getLastModifiedDate().toLocalDate();
 
-            if (now.isAfter(lastModifiedDate)) {
+            if (now.isAfter(lastModifiedDate)) { // 쿼리로 한 번에 처리하기!!
                 switch (orderDetail.getOrderStatus()) {
                     case PENDING:
                         updateToInTransit(orderDetail);
@@ -72,6 +76,10 @@ public class OrderStatusScheduler {
     private void updateToReturnCompleted(OrderDetail orderDetail) {
         Optional<Goods> goodsOpt = goodsRepository.findById(orderDetail.getGoodsId());
         goodsOpt.ifPresent(goods -> goods.increaseStock(orderDetail.getQuantity()));
+        // ex) 1~ 10, 10개의 반품 1번은 양말 한개, 2번은 티셔츠 두개, 3번은 양말 세개
+        // -> 실제로는 양말이랑 티셔츠에 대해 쿼리가 2번만 발생하면 된다. (첫번째 개선 방법)
+        // orderDetail을 여러개 받으면 상품ID도 알 수 있으니 In절로 처리 가능 -> Set 같은 자료구조를 활용해서 상품 처리 가능
+        // Map으로 상품ID와 개수를 처리해서
         orderDetail.changeOrderStatus(OrderStatus.RETURN_COMPLETED);
         log.info("Order {} status changed to RETURN_COMPLETED", orderDetail.getId());
     }
