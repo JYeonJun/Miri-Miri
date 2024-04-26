c81fe4e1ceaadbb4a6035e51b06b8ed9aa0ca982
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2024-04-26 14:41:24 +0900

    [goods-service] eureka client 설정

--- miri-miri-msa/goods-service/src/main/java/com/miri/goodsservice/GoodsServiceApplication.java
+++ miri-miri-msa/goods-service/src/main/java/com/miri/goodsservice/GoodsServiceApplication.java
@@ -2,22 +2,15 @@ package com.miri.goodsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableJpaAuditing
 @SpringBootApplication
-@EnableScheduling
+@EnableDiscoveryClient
 public class GoodsServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GoodsServiceApplication.class, args);
-    }
-
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 }
