d780b0ff6216e9d6441a696a9e16101bd1c5e528
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2024-05-08 13:55:21 +0900

    feat: 상품 주문 이벤트 메시지 주소, 추적id 필드 추가

--- miri-miri-msa/core-module/src/main/java/com/miri/coremodule/dto/kafka/OrderRequestEventDto.java
+++ miri-miri-msa/core-module/src/main/java/com/miri/coremodule/dto/kafka/OrderRequestEventDto.java
@@ -1,8 +1,7 @@
 package com.miri.coremodule.dto.kafka;
 
-import java.io.Serializable;
+import java.util.UUID;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
@@ -11,9 +10,21 @@ import lombok.ToString;
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
-public class OrderRequestEventDto implements Serializable {
+public class OrderRequestEventDto {
     private Long userId;
     private Long goodsId;
     private Integer quantity;
     private Integer goodsPrice;
+    private String address;
+    private String traceId;
+
+    public OrderRequestEventDto(Long userId, Long goodsId, Integer quantity,
+                                Integer goodsPrice, String address, String traceId) {
+        this.userId = userId;
+        this.goodsId = goodsId;
+        this.quantity = quantity;
+        this.goodsPrice = goodsPrice;
+        this.address = address;
+        this.traceId = traceId;
+    }
 }
