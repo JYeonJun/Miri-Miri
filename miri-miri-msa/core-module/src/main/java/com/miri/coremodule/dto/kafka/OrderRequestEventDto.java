29fdb9de910451632b9a5eb406a33b60c866d86a
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2024-05-08 14:38:00 +0900

    fix(goods-service): OrderRequestEventDto 생성자 문제 해결
    @AllArgsConstructor 어노테이션만 남기고 생성자 삭제

--- miri-miri-msa/core-module/src/main/java/com/miri/coremodule/dto/kafka/OrderRequestEventDto.java
+++ miri-miri-msa/core-module/src/main/java/com/miri/coremodule/dto/kafka/OrderRequestEventDto.java
@@ -1,6 +1,5 @@
 package com.miri.coremodule.dto.kafka;
 
-import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,14 +16,4 @@ public class OrderRequestEventDto {
     private Integer goodsPrice;
     private String address;
     private String traceId;
-
-    public OrderRequestEventDto(Long userId, Long goodsId, Integer quantity,
-                                Integer goodsPrice, String address, String traceId) {
-        this.userId = userId;
-        this.goodsId = goodsId;
-        this.quantity = quantity;
-        this.goodsPrice = goodsPrice;
-        this.address = address;
-        this.traceId = traceId;
-    }
 }
