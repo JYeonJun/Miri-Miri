2d19558ad56289ce28ccf31439bece7edd4c4508
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2024-05-13 15:50:17 +0900

    refactor: exception handler 재고 관련 예외 응답 수정
    애러 응답 대신 ok 반환

--- miri-miri-msa/core-module/src/main/java/com/miri/coremodule/handler/CustomExceptionHandler.java
+++ miri-miri-msa/core-module/src/main/java/com/miri/coremodule/handler/CustomExceptionHandler.java
@@ -5,6 +5,7 @@ import com.miri.coremodule.handler.ex.CustomApiException;
 import com.miri.coremodule.handler.ex.CustomValidationException;
 import com.miri.coremodule.handler.ex.EmailAlreadyExistsException;
 import com.miri.coremodule.handler.ex.OrderNotAvailableException;
+import com.miri.coremodule.handler.ex.StockNotFoundException;
 import com.miri.coremodule.handler.ex.StockUnavailableException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -25,12 +26,20 @@ public class CustomExceptionHandler {
 
     @ExceptionHandler({
             CustomApiException.class,
+            StockNotFoundException.class
+    })
+    public ResponseEntity<?> handleCustomApiExceptions(RuntimeException e) {
+        log.error(e.getMessage());
+        return new ResponseEntity<>(new ResponseDto<>(-1, e.getMessage(), null), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler({
             StockUnavailableException.class,
             OrderNotAvailableException.class,
             EmailAlreadyExistsException.class
     })
     public ResponseEntity<?> handleApiExceptions(RuntimeException e) {
         log.error(e.getMessage());
-        return new ResponseEntity<>(new ResponseDto<>(-1, e.getMessage(), null), HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(new ResponseDto<>(1, e.getMessage(), null), HttpStatus.OK);
     }
 }
