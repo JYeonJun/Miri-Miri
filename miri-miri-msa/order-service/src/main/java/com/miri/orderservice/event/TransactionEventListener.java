d974faefa3f3a4468967929a3fbcfe1ce690cef9
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2024-05-14 15:44:09 +0900

    rename(order-service0: OrderToPaymentEventListener -> TransactionEventListener

--- miri-miri-msa/order-service/src/main/java/com/miri/orderservice/event/TransactionEventListener.java
+++ miri-miri-msa/order-service/src/main/java/com/miri/orderservice/event/TransactionEventListener.java
@@ -1,5 +1,6 @@
 package com.miri.orderservice.event;
 
+import com.miri.coremodule.dto.kafka.CancelOrderEventDto;
 import com.miri.coremodule.dto.kafka.OrderRequestEventDto;
 import com.miri.coremodule.dto.kafka.PaymentRequestEventDto;
 import com.miri.coremodule.dto.kafka.StockRollbackEventDto;
@@ -12,16 +13,16 @@ import org.springframework.transaction.event.TransactionalEventListener;
 
 @Slf4j
 @Component
-public class OrderToPaymentEventListener {
+public class TransactionEventListener {
 
     private final KafkaSender kafkaSender;
 
-    public OrderToPaymentEventListener(KafkaSender kafkaSender) {
+    public TransactionEventListener(KafkaSender kafkaSender) {
         this.kafkaSender = kafkaSender;
     }
 
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
-    public void onOrderSuccess(OrderToPaymentEvent event) {
+    public void onOrderSuccess(ProcessOrderEvent event) {
         OrderRequestEventDto orderRequestEventDto = event.getOrderRequestEventDto();
         log.info("traceId={}, 카프카 결제 요청 이벤트 발행", orderRequestEventDto.getTraceId());
         kafkaSender.sendPaymentRequestEvent(KafkaVO.PAYMENT_REQUEST_TOPIC,
@@ -29,10 +30,24 @@ public class OrderToPaymentEventListener {
     }
 
     @TransactionalEventListener(phase = TransactionPhase.AFTER_ROLLBACK)
-    public void onOrderFailure(OrderToPaymentEvent orderToPaymentEvent) {
-        OrderRequestEventDto orderRequestEventDto = orderToPaymentEvent.getOrderRequestEventDto();
+    public void onOrderFailure(ProcessOrderEvent event) {
+        OrderRequestEventDto orderRequestEventDto = event.getOrderRequestEventDto();
         log.info("traceId={}, 주문 실패 이벤트 발행", orderRequestEventDto.getTraceId());
         kafkaSender.sendRollbackRequestEvent(KafkaVO.STOCK_ROLLBACK_TOPIC,
                 StockRollbackEventDto.fromOrderRequest(orderRequestEventDto));
+    }
+
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    public void onOrderStatusToCanceledSuccess(CancelOrderEvent event) {
+        log.info("카프카주문 취소 이벤트 발행");
+        kafkaSender.sendCancelOrderEvent(KafkaVO.CANCEL_ORDER_TOPIC,
+                new CancelOrderEventDto(event.getOrderId(), event.getGoodsId(), event.getQuantity()));
+    }
+
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    public void onReturnSuccess(ReturnEvent event) {
+        log.info("카프카 반품 완료 이벤트 발행");
+        kafkaSender.sendRollbackRequestEvent(KafkaVO.STOCK_ROLLBACK_TOPIC,
+                new StockRollbackEventDto(event.getGoodsId(), event.getQuantity(), null));
     }
 }
